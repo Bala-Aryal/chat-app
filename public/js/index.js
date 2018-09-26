0861440bead5468314dca1684ddda0d43de499ef
Bala <bala_aryal123@hotmail.com>
2018-09-26 13:21:06 -0400

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function (){
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'John',
-    text: 'hey whats up?'
-  });
 });
 
 socket.on('disconnect', function(){
